8c7a1c10abd2a9303a92965d6d8fb09c922dd13e
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-01-15 10:30:00 -0800

    Complete javascript exercises 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if (firstNum < 0 || secondNum < 0) {
+        return 'ERROR';
+    }
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+        return 'ERROR';
+    }
 
+    let bigNum;
+    let smallNum;
+    let sum = 0;
+
+    if (firstNum > secondNum) {
+        bigNum = firstNum;
+        smallNum = secondNum;
+    } else {
+        bigNum = secondNum;
+        smallNum = firstNum;
+    }
+
+    for (let i = smallNum; i < bigNum + 1; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
