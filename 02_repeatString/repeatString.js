4a110cb74d3c5777fb3c58674ffc36e511012088
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-01-15 09:27:29 -0800

    Complete javascript exercises 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
 
+    let repeat = '';
+    for (let i = 0; i < num; i++) {
+        repeat += str;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
