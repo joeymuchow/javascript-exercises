43fcea7f22d5e577582a898a2d18b9920ce38d24
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-01-15 10:58:34 -0800

    Complete javascript exercises 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let celsiusTemp = (fTemp - 32)*(5/9);
+  return Number(celsiusTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fahrenheitTemp = cTemp * (9/5) + 32;
+  return Number(fahrenheitTemp.toFixed(1));
 };
 
 // Do not edit below this line
