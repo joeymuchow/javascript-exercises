1c49e083c06cd47e934f12a83671d21da4a8df11
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-01-15 10:13:03 -0800

    Complete javascript exercises 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeArgs) {
+    const changedArr = arr.filter((item) => {
+        let keepItem = true;
+        for (arg of removeArgs) {
+            if (item === arg) {
+                keepItem = false;
+            }
+        }
+        return keepItem;
+    });
+    return changedArr;
 };
 
 // Do not edit below this line
