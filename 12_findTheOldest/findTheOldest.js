11c0fdf1a372f07f29175c86dc85e5705602a6f4
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-01-17 20:16:44 -0800

    Complete javascript exercises 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const sortedArray = people.sort((a, b) => {
+        const deathA = a.yearOfDeath || new Date().getFullYear();
+        const deathB = b.yearOfDeath || new Date().getFullYear();
+        const ageA = deathA - a.yearOfBirth;
+        const ageB = deathB - b.yearOfBirth;
+        if (ageA > ageB) {
+            return -1;
+        } else if (ageA < ageB) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+    return sortedArray[0];
 };
 
 // Do not edit below this line
