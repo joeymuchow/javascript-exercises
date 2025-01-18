9c111ff717f4a5531d9d49b96fbc73003f3db759
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-01-17 16:17:30 -0800

    Complete javascript exercises 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(first, second) {
+	return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+const power = function(first, second) {
+	return Math.pow(first, second);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+	for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
