9f7f33eb4d163bc8b8e90aac47ad5e6dc1f2fd5c
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-01-17 17:27:08 -0800

    Complete javascript exercises 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const parsedNum = Number(num);
+    if (parsedNum < 0) {
+        return "OOPS";
+    }
+    if (parsedNum === 0) {
+        return 0;
+    }
+    const arr = [];
+    for (let i = 0; i < parsedNum; i++) {
+        if (i < 2) {
+            arr.push(1);
+        } else {
+            const sum = arr[i-2] + arr[i-1];
+            arr.push(sum);
+        }
+    }
+    return arr[parsedNum-1];
 };
 
 // Do not edit below this line
