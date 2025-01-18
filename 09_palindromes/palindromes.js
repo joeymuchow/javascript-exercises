e28cc8c7fcfa22d3baab39ecc6234885ee0b3f91
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-01-17 16:54:24 -0800

    Complete javascript exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const trimmedString = str.replace(/\s+/g, "")
+    .split("")
+        .filter((val) => {
+            return (val.toLowerCase() !== val.toUpperCase()) || Number.isInteger(Number(val));
+        })
+        .join("");
+    const reverse = trimmedString.split("").reverse().join("");
+    return trimmedString.toLowerCase() === reverse.toLowerCase();
 };
 
 // Do not edit below this line
